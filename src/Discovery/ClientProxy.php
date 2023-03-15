15fcb188c578ce7fcd0186e27e1396f9649c96eb
Dimitris Bozelos <dbozelos@gmail.com>
2023-03-15 23:15:49 +0000

    Added integration with the API iterator

--- src/Discovery/ClientProxy.php
+++ src/Discovery/ClientProxy.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace KrystalCode\Acumatica\Api\Discovery;
 
+use KrystalCode\ApiIterator\ClientInterface as IteratorClientInterface;
+use KrystalCode\ApiIterator\Iterator as ApiIterator;
+
 /**
  * Proxy for Acumatica API objects.
  *
@@ -15,8 +18,14 @@ namespace KrystalCode\Acumatica\Api\Discovery;
  *
  * We therefore use a proxy object that allows calling the operation by its ID
  * instead e.g. `getById` instead.
+ *
+ * Moreover, we add a `list` method that integrates with the API iterator that
+ *  makes it easy to browse multiple pages of entities fetched using `getList`
+ * endpoints. Thus, this class acts as a decorator as well in that it adds
+ * functionality. For now it is fine to keep both here; if this further expands
+ * we may want to take this off to a separate decorator class.
  */
-class ClientProxy
+class ClientProxy implements IteratorClientInterface
 {
     /**
      * Constructs a new ClientProxy object.
@@ -49,6 +58,10 @@ class ClientProxy
      */
     public function __call(string $name, mixed $arguments)
     {
+        if (method_exists($this, $name)) {
+            return $this->{$name}(...$arguments);
+        }
+
         if (method_exists($this->client, $name)) {
             return $this->client->{$name}(...$arguments);
         }
@@ -62,5 +75,87 @@ class ClientProxy
             'Unknown method %s',
             $name
         ));
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * It is recommended to always request entities between two timestamps on
+     * the `LastModified` field. Otherwise new entities can be added to the
+     * results while iterating over the pages and resulting in some entities
+     * appearing twice.
+     *
+     * @I Support caching pages in the iterator
+     *    type     : improvement
+     *    priority : low
+     *    labels   : cache, performance
+     */
+    public function list(array $options = [], array $query = [])
+    {
+        $options = array_merge(
+            [
+                'page' => 1,
+                'limit' => 100,
+                'delay' => null,
+            ],
+            $options
+        );
+        $query = array_merge(
+            [
+                'select' => null,
+                'filter' => null,
+                'expand' => null,
+                'custom' => null,
+            ],
+            $query
+        );
+        $query['skip'] = ($options['page'] - 1) * $options['limit'];
+        $query['top'] = $options['limit'];
+
+        // By default, we return an iterator. Caller code can then loop over it
+        // and the iterator will internally handle making the API calls for
+        // fetching the objects for each page.
+        if (empty($options['bypass_iterator'])) {
+            return new ApiIterator(
+                $this,
+                $options['page'],
+                $options['limit'],
+                $query,
+                false,
+                $options['delay']
+            );
+        }
+
+        // Otherwise, either it is requested to fetch the results directly, or
+        // this is a call by the iterator while being looped over. Make the
+        // request and return the results.
+        unset($options['bypass_iterator']);
+
+        $objects = $this->getList(
+            $query['select'],
+            $query['filter'],
+            $query['expand'],
+            $query['custom'],
+            $query['skip'],
+            $query['top']
+        );
+        if ($objects === null) {
+            $objects = [];
+        }
+
+        $has_more = null;
+        $count = count($objects);
+        if ($count === 0 || $count < $options['limit']) {
+            $has_more = false;
+        }
+
+        return [
+            new \CachingIterator(
+                new \ArrayIterator($objects),
+                \CachingIterator::FULL_CACHE
+            ),
+            $has_more,
+            $query,
+        ];
     }
 }
